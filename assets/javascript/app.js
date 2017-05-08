c3c2755d87d5ef4afe4219c9c96c28074bc8ffda
Prabhdeep <ps591990@gmail.com>
2017-05-08 02:25:34 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,53 +23,113 @@ var answers = [["Jim Halpert", "Ryan Howard", "Michael Scott", "Erin Hannon"], [
 "Legends of the Fall, Legally Blonde, Bridges of Madison County, My Big Fat Greek Wedding, Ghost (but just that one scene)",
 "Fargo, Edward Scissor-hands, The Breakfast Club, Dazed and Confused, The Princess Bride"]];
 
-var correctAnswers = ["B. Ryan Howard", "A. Chris Rock", "D. Hair Cancer", "C. $25", "D. Toaster Oven", "D. Katy", "B. The Bushiest Beaver Dundie",
- "B. WLHUNG", "A. Flonkerton", "C. Legends of the Fall, Legally Blonde, Bridges of Madison County, My Big Fat Greek Wedding, Ghost (but just that one scene)"]
+var correctAnswers = ["Ryan Howard", "Chris Rock", "Hair Cancer", "$25", "Toaster Oven", "Katy", "The Bushiest Beaver Dundie",
+ "WLHUNG", "Flonkerton", "Legends of the Fall, Legally Blonde, Bridges of Madison County, My Big Fat Greek Wedding, Ghost (but just that one scene)"]
 
 
 
 
-$(".question").html("<h2>"+questions[0]+"</h2>")
-$(".answer1").html("<h3>"+answers[0][0]+"</h3>")
-$(".answer2").html("<h3>"+answers[0][1]+"</h3>")
-$(".answer3").html("<h3>"+answers[0][2]+"</h3>")
-$(".answer4").html("<h3>"+answers[0][3]+"</h3>")
-
-
-
-
-
+// $(".question").html("<h2>"+questions[0]+"</h2>")
+// $(".answer1").html("<h3>"+answers[0][0]+"</h3>")
+// $(".answer2").html("<h3>"+answers[0][1]+"</h3>")
+// $(".answer3").html("<h3>"+answers[0][2]+"</h3>")
+// $(".answer4").html("<h3>"+answers[0][3]+"</h3>")
 
 
 
 
 
 
+// Variable showImage will hold the setInterval when we start the slideshow
+var showImage;
+
+// Count will keep track of the index of the currently displaying picture.
+var count = 0;
+
+// TODO: Use jQuery to run "startSlideshow" when we click the "start" button.
+$("#start").click(startSlideshow);
 
 
 
 
+// This function will replace display whatever image it's given
+// in the 'src' attribute of the img tag.
+function displayQuestion() {
+  $(".question").text(questions[count]);
+  $(".answer1").text(answers[count][0]);
+   $(".answer2").text(answers[count][1]);
+    $(".answer3").text(answers[count][2]);
+     $(".answer4").text(answers[count][3]);
+}
 
+function nextQuestion() {
+  //  TODO: Increment the count by 1.
+  count++;
+//   setTimeout(timer, 1000);
 
+  // TODO: Show the loading gif in the "image-holder" div.
+  $(".question").empty();
+  $(".answer1").empty();
+   $(".answer2").empty();
+    $(".answer3").empty();
+     $(".answer4").empty();
 
+  // TODO: Use a setTimeout to run displayImage after 1 second.
+  setTimeout(displayQuestion, 1000);
 
+  // TODO: If the count is the same as the length of the image array, reset the count to 0.
+  if (count === questions.length) {
+    count = 0;
+  }
+}
 
+function startSlideshow() {
 
+  // TODO: Use showImage to hold the setInterval to run nextImage.
+  showImage = setInterval(nextQuestion, 10000);
 
-
-var start = 10;
-
-function timer() {
-    $("#clock").text(start)
-    start--;
-
-    if (start < 0) {
-        $("#clock").html("<p>Times Up!!</p>")
-    }
 }
 
 
-setInterval(timer, 1000)
+
+// This will run the display image function as soon as the page loads.
+displayQuestion();
+startSlideshow();
+
+
+$(".answer").on("click", function(event){
+    console.log(correctAnswers[count])
+	//answeredQuestion = true;
+	
+	selectedAnswer = $(this).text();
+    console.log(selectedAnswer);
+	if(selectedAnswer === correctAnswers[count]) {
+		alert("correct");
+    }
+	else {
+		alert("wrong answer!");
+    }
+		
+}); // Close .answer click
+
+
+// var start = 10;
+
+// function timer() {
+//     $("#clock").text(start)
+
+    
+//     start--;
+   
+  
+//     if (start < 0) {
+//         // $("#clock").html("<p>Times Up!!</p>");
+//         start = 10;
+//     }
+// }
+
+
+// setInterval(timer, 1000)
 
 
 
