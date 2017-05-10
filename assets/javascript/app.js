790108b189c46b224d1872c133563bb0621c8b9a
Prabhdeep <ps591990@gmail.com>
2017-05-10 00:15:50 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,8 +20,8 @@ $(document).ready(function () {
                     "In S1E6 'Hot Girl' What is the Hot Girl's name?",
                     "In S2E1 'The Dundies': What Dundie award does Phyllis take home?",
                     "In S2E2 'Sexual Harassment': What is on Todd Packer's vanity license plate?",
-                    "In S2E3 'Office Olympics': What does Pam name 'Box of paper snowshoe racing'?",
-                    "In S2E4 'The Fire': What are Meredith's five DVD choices for the game 'Desert Island'?"
+                    "In S3E9 'The Convict': What is the worst thing about prison according to Prison Mike'?",
+                    "In S2E5 'Halloween': What is Jim's costume?"
                     ],
 
         answers: [
@@ -29,27 +29,25 @@ $(document).ready(function () {
                   ["Killer Nanorobots", "Hot Dog Fingers", "Count Choculytise", "Hair Cancer"], ["$40", "$10", "$25", "$100"],
                   ["Toaster", "Microwave", "Blender", "Toaster Oven"], ["Amy", "Christy", "Kelly", "Katy"],
                   ["The Busiest Beaver Dundie", "The Bushiest Beaver Dundie", "Spicy Curry Dundie", "Whitest Sneaker Dundie"],
-                  ["LUVMKR", "WLHUNG", "TODPKR", "BGDADY"], ["Flonkerton", "Icelandic Snowshoe Racing", "Bixinng", "Pergerhosen"],
-                  ["The Shawshank Redemption, 40 Year Old Virgin, Gentlemen Prefer Blondes, Disney's Sleeping Beauty, Life of Pi",
-                   "Gone With The Wind, The Burbs, Clerks II, Sense & Sensibility, Chronicles of Riddick",
-                   "Legends of the Fall, Legally Blonde, Bridges of Madison County, My Big Fat Greek Wedding, Ghost (but just that one scene)",
-                   "Fargo, Edward Scissor-hands, The Breakfast Club, Dazed and Confused, The Princess Bride"]
+                  ["LUVMKR", "WLHUNG", "TODPKR", "BGDADY"], ["Gruel", "Dementors", "Gruel Sandwiches", "Eating Hair"],
+                  ["Dave", "Bookface","Three Hole Punch Jim", "Rational Consumer"]
                 ],
 
         correctAnswers: ["Ryan Howard", "Chris Rock", "Hair Cancer", "$25", "Toaster Oven", "Katy", "The Bushiest Beaver Dundie",
-                        "WLHUNG", "Flonkerton", "Legends of the Fall, Legally Blonde, Bridges of Madison County, My Big Fat Greek Wedding, Ghost (but just that one scene)"
+                        "WLHUNG", "Dementors", "Bookface"
                         ],
-        videoAnswers: [ "https://www.youtube.com/embed/4Q12Spp6mnI?autoplay=1", "https://www.youtube.com/embed/aeQIQuDL8aA?autoplay=1", "https://www.youtube.com/embed/ifFzxiSQsCE?start=52&end=102&autoplay=1", "https://www.youtube.com/embed/hX1cX9sxygA?autoplay=1",
+        videoAnswers: [ "https://www.youtube.com/embed/4Q12Spp6mnI?autoplay=1", "https://www.youtube.com/embed/aeQIQuDL8aA?start=18&autoplay=1", "https://www.youtube.com/embed/ifFzxiSQsCE?start=52&end=102&autoplay=1", "https://www.youtube.com/embed/hX1cX9sxygA?autoplay=1",
                "https://www.youtube.com/embed/eJGov6c1VmU?autoplay=1", "https://www.youtube.com/embed/YOjK1_JvZHQ?autoplay=1", "https://www.youtube.com/embed/vYb9XjZglSw?autoplay=1", "https://www.youtube.com/embed/6xST2zjX0UI?start=150&end=159&autoplay=1",
-               "https://goo.gl/cIHCsV","https://goo.gl/4hBky4"],      
+               "https://www.youtube.com/embed/a7RoP1LKMeM?start=132&end=152&autoplay=1","https://www.youtube.com/embed/PQeXEYsPOZI?autoplay=1",],      
 
-
+     
               
 
-        videoLength: [19000, 43000, 50000, 18000, 14000, 31000, 13000, 9000, 14000, 49000],
+        videoLength: [20000, 27000, 50000, 18000, 14000, 31000, 13000, 9000, 21000, 9000],
+
         startGame: function() {
             $("#start").click(function() {
-                object.displayQuestion();
+                object.displayQuestion()
                 //Need to work on timer//
             });
         },
@@ -74,14 +72,30 @@ $(document).ready(function () {
 
      correctSelection: function() {
         correctAnswers++;
-          $(".question").html("<h2>Correct!!</h2>");
-          $(".mainArea").html("<h2 class='text-center'>Correct!</h2>" + "<iframe class='text-center' width='420' height='345' style=' position:relative; left:361px; top:30px' src='"+object.videoAnswers[count]+"'></iframe>"+"<br><button class='nextQuestion'>Next Question</button>")
-
-setTimeout(object.wait, 4000);
+       
+        //   $(".question").html("<h2>Correct!!</h2>");
+          $(".results").html("<h2 class='text-center'>Correct!</h2>" + "<iframe class='text-center' width='' height='345' src='"+object.videoAnswers[count]+"'></iframe>"+"<br><button class='nextQuestion'>Next Question</button>");
+           $(".answer1").empty();
+            $(".answer2").empty();
+            $(".answer3").empty();
+            $(".answer4").empty();
+   
+    
+        // count++;
+        setTimeout(object.nextQuestion, object.videoLength[count]);
+        console.log()
+        
     },
 
     wrongSelection: function() {
-setTimeout(object.wait, 4000);
+        wrongAnswers++;
+         $(".question").html("<h2>Inorrect!!</h2>");
+         $(".results").html("<h2 class='text-center'>Incorrect!</h2>" + "<img src='assets/images/no.gif'>"+"<br><button class='nextQuestion'>Next Question</button>");
+          $(".answer1").empty();
+            $(".answer2").empty();
+            $(".answer3").empty();
+            $(".answer4").empty();
+        setTimeout(object.nextQuestion, 5000);
     },
 
 
@@ -92,45 +106,38 @@ setTimeout(object.wait, 4000);
             $(".answer2").text(object.answers[count][1]);
             $(".answer3").text(object.answers[count][2]);
             $(".answer4").text(object.answers[count][3]);
+            $(".results").empty();
+
+            // object.pickingAnswer();
 
           
         },
 
-        startSlideshow: function () {
-
-            showImage = setInterval(object.nextQuestion, 6000);
-        },
-
         nextQuestion: function () {
-       
-            console.log(count)
 
-            $(".question").empty();
+              if (count < 9) {
+	        count++;
+            console.log(count)
+	        object.displayQuestion();
+	        // counter = 5;
+	        // object.timer();
+	    }
+	    else {
+		    object.endScreen();
+	    }
+       
+            
+        },
+        endScreen: function() {
+            $(".results").html("<h2>Game Over</h2><h3>Correct Answers:"+ correctAnswers +"</h3><h3>Wrong Answers:"+ wrongAnswers +"</h3>")
+             $(".question").empty();
             $(".answer1").empty();
             $(".answer2").empty();
             $(".answer3").empty();
             $(".answer4").empty();
-
-            setTimeout(object.displayQuestion, 1000);
-             count++;
-            if (count === object.questions.length) {
-                count = 0;
-            }
         },
 
 
-
-    wait: function wait() {
-	        if (count < 10) {
-	        count++;
-	        object.displayQuestion();
-	        counter = 5;
-	        // object.timer();
-	    }
-	    else {
-		    // finalScreen();
-	    }
-},
         timer: function() {
 
              $("#clock").html(clock);
@@ -142,14 +149,12 @@ setTimeout(object.wait, 4000);
         }
 
     }
-  object.displayQuestion();
-// setInterval(object.timer, 1000);
+
 
    
-  
-
-    object.startSlideshow();
-    object.nextQuestion();
-    object.pickingAnswer();
+  object.startGame();
+object.pickingAnswer();
+//   object.nextQuestion();
+    
 });
 
